4c0ec7d200b89c24f4ef5fd92a637964824ba8c3
gabrielsmm <gabrielsmm251@gmail.com>
2023-03-13 21:38:02 -0300

    Mentoria e Curso

---
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,53 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.Date;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private Date data;
+
+    public Mentoria() {
+
+    }
+
+    public Mentoria(String titulo, String descricao, Date data) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
